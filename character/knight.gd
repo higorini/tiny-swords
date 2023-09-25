7466b52a94375cf05f4f6df6dae88e80b712c96a
higorini <108697425+higorini@users.noreply.github.com>
2023-09-25 15:25:14 -0300

    Movimentação e ataque com animação in-game

--- character/knight.gd
+++ character/knight.gd
@@ -1,12 +1,27 @@
 extends CharacterBody2D
 
+@onready var animation: AnimationPlayer = get_node("Animation")
+@onready var texture: Sprite2D = get_node("Texture")
+
 @export var move_speed: float = 256.0
 
+var can_attack: bool = true
+
+
 func _physics_process(_delta: float) -> void:
+	if can_attack == false:
+		return
+		
+	move()
+	animate()
+	attack_handler()
+	
+
+
+func move() -> void:
 	var direction: Vector2 = get_direction()
 	velocity = direction * move_speed
 	move_and_slide()
-
 
 
 func get_direction() -> Vector2:
@@ -14,3 +29,27 @@ func get_direction() -> Vector2:
 		Input.get_axis("move_left", "move_right"),
 		Input.get_axis("move_up", "move_down")
 	)
+
+
+func animate() -> void:
+	if velocity.x > 0:
+		texture.flip_h = false
+		
+	if velocity.x < 0:
+		texture.flip_h = true
+	
+	if velocity != Vector2.ZERO:
+		animation.play("run")
+		return
+		
+	animation.play("idle")
+
+
+func attack_handler() -> void:
+	if Input.is_action_just_pressed("attack_button") and can_attack:
+		can_attack = false
+		animation.play("attack")
+
+
+func _on_animation_finished(_anim_name: String) -> void:
+	can_attack = true
